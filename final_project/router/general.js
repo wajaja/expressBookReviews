e96ea1757e81f7f0f1673d2adfb33eeea03e9611
wajaja <cedrickngeja@gmail.com>
2025-04-12 12:24:33 -0400

    add auth3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,12 +19,16 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]);
+    //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author = req.params.author;
+    return res.send(books[author]);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
