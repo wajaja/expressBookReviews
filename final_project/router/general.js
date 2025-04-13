6df5212fc96019306f54e18ddb73f66efcecf17b
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-04-13 21:26:53 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,27 +35,26 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  new Promise((resolve, reject) => {
-    const isbn = req.params.isbn
-    const book = books[isbn]
-    if (!book) {
-      reject('Book not found')
-    } else {
-      resolve(book)
-    }
-  })
-    .then((data) => {
+     const get_books_isbn = new Promise((resolve, reject) => {
+       const isbn = req.params.isbn
+       const book = books[isbn]
+       if (!book) {
+            reject('Book not found')
+         } else {
+            resolve(book)
+         }
+     });
+    get_books_isbn.then((data) => {
       res.status(200).json(data)
     })
     .catch((error) => {
       res.status(404).json({ message: error })
     })
-    
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  new Promise((resolve, reject) => {
+  const get_books_auth = new Promise((resolve, reject) => {
     const author = req.params.author
     const booksByAuthor = Object.values(books).filter(
       (b) => b.author === author
@@ -65,8 +64,8 @@ public_users.get('/author/:author',function (req, res) {
     } else {
       resolve({ booksbyauthor: booksByAuthor })
     }
-  })
-    .then((data) => {
+  });
+    get_books_auth.then((data) => {
       res.status(200).json(data)
     })
     .catch((error) => {
@@ -76,7 +75,7 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  new Promise((resolve, reject) => {
+  const get_books_title = new Promise((resolve, reject) => {
     const title = req.params.title
     const booksByTitle = Object.values(books).filter((b) =>
       b.title.includes(title)
@@ -86,8 +85,8 @@ public_users.get('/title/:title',function (req, res) {
     } else {
       resolve({ booksbytitle: booksByTitle })
     }
-  })
-    .then((data) => {
+  });
+    get_books_title.then((data) => {
       res.status(200).json(data)
     })
     .catch((error) => {
